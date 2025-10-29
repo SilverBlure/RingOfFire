94b64af438c5546908cc3d3ee321ef4d07caac22
Nicolai Österle <oesterle.ni@onlinehome.de>
2025-10-29 20:36:04 +0100

    add new class

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,20 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game!: Game;
 
-pickCardAnimation  = false;
+  constructor() {}
 
-drawCard(){
-  this.pickCardAnimation = true;
-}
+  ngOnInit(): void {
+    this.newGame();
+   
+  }
 
+  newGame() {
+    this.game = new Game();
+ console.log(this.game);
+  }
 
+  drawCard() {
+    this.pickCardAnimation = true;
+  }
 }
